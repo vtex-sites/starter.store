b184b8e7c032daf5f5a0e8be749787b281fc548b
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-01-26 13:13:01 -0300

    chore:attempts to merge store configs

--- faststore.config.js
+++ faststore.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  account: 'storeframework',
-}
+  account: "storeframework",
+  // Lighthouse CI
+  lighthouse: {
+    server: () => process.env.BASE_SITE_URL || "http://localhost:3000",
+    pages: {
+      home: "/",
+      pdp: "/apple-magic-mouse/p",
+      collection: "/office",
+    },
+  },
+  // E2E CI
+  cypress: {
+    pages: {
+      home: "/",
+      pdp: "/apple-magic-mouse/p",
+      collection: "/office",
+      collection_filtered:
+        "/office/?category-1=office&marca=acer&facets=category-1%2Cmarca",
+      search: "/s?q=orange",
+    },
+  },
+};
