178a1bec6ddaca6bc4419870bc05e62fd1a6bc9d
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-02-08 19:13:14 -0300

    chore: updates faststore.config.js

--- faststore.config.js
+++ faststore.config.js
@@ -1,16 +1,43 @@
 module.exports = {
   account: "storeframework",
-  theme: "soft-blue",
-  // Lighthouse CI
+  theme: "custom-theme",
+  seo: {
+    title: "NextJSStore",
+    description: "Fast Demo Store",
+    titleTemplate: "%s | FastStore",
+    author: "Store Framework",
+  },
+  platform: "vtex",
+  api: {
+    storeId: "storeframework",
+    workspace: "master",
+    environment: "vtexcommercestable",
+    hideUnavailableItems: true,
+  },
+  session: {
+    currency: {
+      code: "USD",
+      symbol: "$",
+    },
+    locale: "en-US",
+    channel: '{"salesChannel":"1","regionId":""}',
+    country: "USA",
+    postalCode: null,
+    person: null,
+  },
+  storeUrl: "https://vtexfaststore.com",
+  secureSubdomain: "https://secure.vtexfaststore.com",
+  checkoutUrl: "https://secure.vtexfaststore.com/checkout",
+  loginUrl: "https://secure.vtexfaststore.com/api/io/login",
+  accountUrl: "https://secure.vtexfaststore.com/api/io/account",
   lighthouse: {
-    server: process.env.BASE_SITE_URL || "http://localhost:3000",
+    server: "http://localhost:3000",
     pages: {
       home: "/",
       pdp: "/apple-magic-mouse/p",
       collection: "/office",
     },
   },
-  // E2E CI
   cypress: {
     pages: {
       home: "/",
@@ -20,5 +47,8 @@ module.exports = {
         "/office/?category-1=office&marca=acer&facets=category-1%2Cmarca",
       search: "/s?q=orange",
     },
+  },
+  analytics: {
+    gtmContainerId: "GTM-PGHZ95N",
   },
 };
