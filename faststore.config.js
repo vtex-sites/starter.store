91a534db2c67d70dd6257046fb08fd6a1a7ea4d7
Eduardo Formiga <eduardo.formiga@gmail.com>
2023-02-14 11:39:35 -0300

    chore: WebOps Evergreen + Vercel (#12)
    * chore: updates cli version
    
    * chore: updates core version
    
    * chore: attempt to fix lint on vercel
    
    * chore: attempts to update msw (temporary)
    
    * chore: attempts to change the build dir
    
    * chore: attempts to use next
    
    * Trigger build
    
    * Trigger build
    
    * chore: updates vercel outputDirectory
    
    * Trigger build
    
    * Trigger build
    
    * Trigger build
    
    * chore: removes comment from next config
    
    * chore: updates core version
    
    * chore: trigger CI after copy .next folder to root
    
    * chore: updates cli version
    
    * chore: updates cli version
    
    * chore: updates gitignore
    
    * chore: updates gitignore
    
    * Trigger build
    
    * chore: attempts to use outputFileTracingRoot
    
    * chore: reverts: attempts to use outputFileTracingRoot
    
    * chore: changes the start command
    
    * chore: updates cli version
    
    * chore: attempts to remove vercel configs
    
    * chore: updates cli version
    
    * chore: updates cli version
    
    * chore: updates cli version
    
    * chore: updates cli version
    
    * chore: updates cli version
    
    * chore: updates vercel.json outputDirectory
    
    * chore: updates cli version
    
    * chore: adds dependencies
    
    * chore: updates cli version
    
    * chore:attempts to merge store configs
    
    * chore: attempts to ensure LH with BASE_SITE_URL
    
    * chore: reverts LH server
    
    * chore: updates cli (symlink src)
    
    * chore: remove next config
    
    * Trigger build
    
    * Trigger build
    
    * chore: updates CLI version
    
    * chore: adds cypress.json from the very begining
    
    * chore: udates cli (removes src symlink)
    
    * chore: adds theme again
    
    * chore: adds custom theme as default theme
    
    * Trigger build
    
    * chore: updates cli version
    
    * chore: updates gitignore
    
    * chore: adds empty line to .gitignore
    
    * Trigger build
    
    * Trigger build
    
    * chore: updates faststore.config.js
    
    * chore: updates dependencies
    
    * chore: adds vtex.env
    
    * chore: updates dependencies
    
    * Trigger build
    
    * chore: updates core version
    
    * Trigger build
    
    * chore: updates dependencies
    
    * Trigger build

--- faststore.config.js
+++ faststore.config.js
@@ -1,3 +1,54 @@
 module.exports = {
-  account: 'storeframework',
-}
+  account: "storeframework",
+  theme: "custom-theme",
+  seo: {
+    title: "NextJSStore",
+    description: "Fast Demo Store",
+    titleTemplate: "%s | FastStore",
+    author: "Store Framework",
+  },
+  platform: "vtex",
+  api: {
+    storeId: "storeframework",
+    workspace: "master",
+    environment: "vtexcommercestable",
+    hideUnavailableItems: true,
+  },
+  session: {
+    currency: {
+      code: "USD",
+      symbol: "$",
+    },
+    locale: "en-US",
+    channel: '{"salesChannel":"1","regionId":""}',
+    country: "USA",
+    postalCode: null,
+    person: null,
+  },
+  storeUrl: "https://vtexfaststore.com",
+  secureSubdomain: "https://secure.vtexfaststore.com",
+  checkoutUrl: "https://secure.vtexfaststore.com/checkout",
+  loginUrl: "https://secure.vtexfaststore.com/api/io/login",
+  accountUrl: "https://secure.vtexfaststore.com/api/io/account",
+  lighthouse: {
+    server: "http://localhost:3000",
+    pages: {
+      home: "/",
+      pdp: "/apple-magic-mouse/p",
+      collection: "/office",
+    },
+  },
+  cypress: {
+    pages: {
+      home: "/",
+      pdp: "/apple-magic-mouse/p",
+      collection: "/office",
+      collection_filtered:
+        "/office/?category-1=office&marca=acer&facets=category-1%2Cmarca",
+      search: "/s?q=orange",
+    },
+  },
+  analytics: {
+    gtmContainerId: "GTM-PGHZ95N",
+  },
+};
