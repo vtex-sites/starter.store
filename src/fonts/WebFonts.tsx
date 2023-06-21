ea9b0b3b24f6e94adf72e0948fc2627b9531bc3a
renatamottam <renatamotta.m@gmail.com>
2023-06-21 11:18:26 -0300

    feat: Removes theme

--- src/fonts/WebFonts.tsx
+++ src/fonts/WebFonts.tsx
@@ -1,10 +1,10 @@
-function WebFonts() {
-  return (
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;900&family=Roboto+Slab:wght@300;400;700;900&display=swap"
-    />
-  )
-}
+// function WebFonts() {
+//   return (
+//     <link
+//       rel="stylesheet"
+//       href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;900&family=Roboto+Slab:wght@300;400;700;900&display=swap"
+//     />
+//   )
+// }
 
-export default WebFonts
+// export default WebFonts
