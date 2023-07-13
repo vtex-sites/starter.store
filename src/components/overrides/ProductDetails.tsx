1caaccff892368bd6bf2b0f329716c960ff92450
Ícaro Azevedo <icazevedo10@gmail.com>
2023-07-13 17:19:49 -0300

    demo prop override

---
+++ src/components/overrides/ProductDetails.tsx
@@ -0,0 +1,12 @@
+import { SectionOverride } from "@faststore/core/src/typings/overrides";
+
+const SECTION = "ProductDetails";
+
+const override: SectionOverride = {
+  section: SECTION,
+  components: {
+    BuyButton: { props: { size: "small", iconPosition: "right" } },
+  },
+};
+
+export { override };
