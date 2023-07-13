bc061b07ff11babb1a291cac2d7fea8c4e5b2f04
renatamottam <renatamotta.m@gmail.com>
2023-07-13 15:13:50 -0300

    feat: Adds custom font

---
+++ src/fonts/WebFonts.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @next/next/no-page-custom-font */
+
+function WebFonts() {
+  return (
+    <>
+      {/* Add a <link> tag for your font-family of choice */}
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;900&family=Roboto+Slab:wght@300;400;700;900&display=swap"
+      />
+    </>
+  );
+}
+
+export default WebFonts;
