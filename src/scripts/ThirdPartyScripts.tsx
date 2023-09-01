1098e8250323f90fa689585b971963389839654b
Guilherme Carvalho <guilherme.carvalho@vtex.com.br>
2023-09-01 10:36:43 -0300

    Add a sample third party script

---
+++ src/scripts/ThirdPartyScripts.tsx
@@ -0,0 +1,6 @@
+const ThirdPartyScripts = () => {
+  return <script type="text/javascript" dangerouslySetInnerHTML={{__html:"console.log('hello from a third-party script')"}}/>
+}
+
+export default ThirdPartyScripts;
+
