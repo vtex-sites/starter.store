24c298283358d41edf0ed4f1c3108b251584666a
eduardoformiga <eduardo.formiga@vtex.com.br>
2023-09-05 15:18:48 -0300

    chore: adds third party api extensions

---
+++ src/graphql/thirdParty/resolvers/extra.ts
@@ -0,0 +1,15 @@
+const extraDataResolver = {
+  Query: {
+    extraData: () => {
+      return {
+        data: 'Extra data',
+      }
+    },
+    namedExtraData: (_, { name }) => {
+      return {
+        data: `Named extra data: ${name}`,
+      }
+    },
+  },
+}
+export default extraDataResolver
