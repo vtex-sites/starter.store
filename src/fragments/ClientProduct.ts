0fab5f7a5d2ebff34f9c0f46d972c8e0591208ef
Guilherme Carvalho <guilherme.carvalho@vtex.com.br>
2024-02-05 11:25:06 -0300

    Update dependencies

--- src/fragments/ClientProduct.ts
+++ src/fragments/ClientProduct.ts
@@ -1,6 +1,6 @@
 import { gql } from '@faststore/core/api'
  
-export const fragment = gql`
+export const fragment = gql(`
   fragment ClientProduct on Query {
     product(locator: $locator) {
       galleryImages: image(context: "gallery", limit: 3) {
@@ -9,4 +9,4 @@ export const fragment = gql`
       }
     }
   }
-`
+`)
