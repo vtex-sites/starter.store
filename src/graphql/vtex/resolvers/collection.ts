013c48611ae5fe6e2d16ad4242590b8fa5c9d63a
eduardoformiga <eduardo.formiga@gmail.com>
2024-04-09 16:08:17 -0300

    chore: attempts to fix build type

--- src/graphql/vtex/resolvers/collection.ts
+++ src/graphql/vtex/resolvers/collection.ts
@@ -1,15 +1,10 @@
-import type { StoreCollectionRoot } from "@faststore/core/api";
-
 const collectionResolver = {
   StoreCollection: {
-    title: (root: StoreCollectionRoot) => root?.title ?? root?.Title ?? "",
-    metaTagDescription: (root: StoreCollectionRoot) =>
+    title: (root: any) => root?.title ?? root?.Title ?? "",
+    metaTagDescription: (root: any) =>
       root?.metaTagDescription ?? root?.MetaTagDescription ?? "",
-    customData: (root: StoreCollectionRoot) => {
-      console.log(
-        "🚀 ~ root StoreCollectionRoot:",
-        JSON.stringify(root, null, 2)
-      );
+    customData: (root: any) => {
+      console.log("🚀 ~ root any:", JSON.stringify(root, null, 2));
       return "https://newstore.vtexassets.com/arquivos/ids/155412/airpods-3.png?v=638466438094530000";
     },
   },
