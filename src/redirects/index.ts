fad4fde01c0151cf78e1f9de5a378e24afe3f28a
Pedro Soares <pedro.soares@vtex.com>
2024-12-10 14:43:13 -0300

    feat: add redirect customization

---
+++ src/redirects/index.ts
@@ -0,0 +1,14 @@
+interface MatcherArgs {
+    pathname: string
+}
+
+const searchRegex = /^\/busca\/([^/]+)/
+// custom matcher for search pages in Americanas case  
+export function matcher({pathname}: MatcherArgs): string | null {
+  const searchMatch = pathname.match(searchRegex)
+  if (searchMatch) {
+    const searchTerm = searchMatch[1].replaceAll('-', '+')
+    return `/s?q=${searchTerm}`
+  }
+  return null
+}
