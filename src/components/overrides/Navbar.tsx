36066f1fb04beeb869ff93b27d71b8fbc485cb87
Fanny Chien <fannychienn@gmail.com>
2025-01-24 20:51:08 -0300

    chore: Removes override test

--- src/components/overrides/Navbar.tsx
+++ src/components/overrides/Navbar.tsx
@@ -1,12 +1,11 @@
 import { SectionOverride } from "@faststore/core";
-import { CustomHeader } from "../CustomHeader";
 
 const SECTION = "Navbar" as const;
 
 const override: SectionOverride = {
   section: SECTION,
   components: {
-    _experimentalButtonSignIn: { Component: CustomHeader },
+    // _experimentalButtonSignIn: { Component: CustomHeader },
   },
 };
 
