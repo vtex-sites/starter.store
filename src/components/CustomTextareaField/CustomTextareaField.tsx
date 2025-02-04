d82be95784394305943ad7b4028d0b8bda6dc89d
Artur Santiago <artursantiago2001@hotmail.com>
2025-02-04 16:03:51 -0300

    feat: add title for section

--- src/components/CustomTextareaField/CustomTextareaField.tsx
+++ src/components/CustomTextareaField/CustomTextareaField.tsx
@@ -5,13 +5,13 @@ import styles from "./customTextareaField.module.scss";
 export default function CustomTextareaField() {
   return (
     <section className={styles.customTextareaField}>
+      <h2>Textarea Field Section</h2>
+      <br />
       <UITextareaField
         id="textarea-field"
         label="Share your thoughts about the product. How would you describe its quality?"
       />
-
       <UITextareaField id="textarea-field-short-label" label="Short label" />
-
       <UITextareaField
         id="textarea-field-error"
         label="Share your thoughts about the product. How would you describe its quality?"
