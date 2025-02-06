2b542621f4437392844412c796d95ee86fd60754
Artur Santiago <artursantiago2001@hotmail.com>
2025-02-06 15:42:01 -0300

    fix: import styles

--- src/components/CustomModalFooterSection/CustomModalFooterSection.tsx
+++ src/components/CustomModalFooterSection/CustomModalFooterSection.tsx
@@ -7,17 +7,30 @@ import {
 } from "@faststore/ui";
 
 import styles from "./customModalFooterSection.module.scss";
+import { useState } from "react";
 
 export default function CustomModalFooterSection() {
+  const [open, setOpen] = useState(false);
+
   return (
     <section className={styles.customModalFooterSection}>
       <h2>Modal Footer Section</h2>
-      <br />
-      <Modal>
-        <ModalHeader title="Add a review" />
+      <Button variant="primary" onClick={() => setOpen(true)}>
+        Open Modal
+      </Button>
+
+      <Modal
+        className={styles.customModal}
+        isOpen={open}
+        onDismiss={() => setOpen(false)}
+        overlayProps={{ className: styles.customOverlay }}
+      >
+        <ModalHeader onClose={() => setOpen(false)} title="Add a review" />
         <ModalBody>Modal Body</ModalBody>
         <ModalFooter>
-          <Button variant="secondary">Cancel</Button>
+          <Button onClick={() => setOpen(false)} variant="secondary">
+            Cancel
+          </Button>
           <Button>Submit your review</Button>
         </ModalFooter>
       </Modal>
