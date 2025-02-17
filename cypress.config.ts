385a5c2e0ec29654debf4ecd7a74ecc26930fc00
eduardoformiga <eduardo.formiga@gmail.com>
2025-02-17 18:42:34 -0300

    feat: test cypress base url

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       return require("cypress/plugins/index.js")(on, config);
     },
-    baseUrl: "http://localhost:3000/",
+    baseUrl: process.env.BASE_URL || "http://localhost:3000/",
     experimentalRunAllSpecs: true,
   },
 });
